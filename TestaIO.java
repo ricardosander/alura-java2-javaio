4c04e09303232f0f4062780317987f588d2b6bd6
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-23 02:05:13 -0300

    Módulo 4, Exercício 6: usando classe Scanner para ler o arquivo.

--- TestaIO.java
+++ TestaIO.java
@@ -1,8 +1,7 @@
-import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.util.Scanner;
 
 public class TestaIO {
 
@@ -10,30 +9,20 @@ public class TestaIO {
 
 		try {
 
-			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/arquivo.txt")));
+			InputStream is = new FileInputStream("src/arquivo.txt");
+			Scanner scanner = new Scanner(is);
 			System.out.println("Olá. Bem vindo ao leitor de arquivo. Lendo arquivo.txt...");
-			try {
 
-				StringBuilder textoBuilder = new StringBuilder();
-				String texto;
-				while ((texto = br.readLine()) != null) {
-					textoBuilder.append(texto);
-					textoBuilder.append("\n");
-				}
-				System.out.println("Texto lido:");
-				System.out.println(textoBuilder.toString());
-			} catch (IOException e) {
-				System.out.println("Houve um erro ao realizar a leitura.\n" + e.getMessage());
-			} finally {
-
-				System.out.println("Tchau tchau.");
-				try {
-					br.close();
-				} catch (IOException e) {
-					System.out.println("Houve um erro ao fechar o leitor.\n" + e.getMessage());
-				}
+			System.out.println("Texto lido:");
+			while (scanner.hasNextLine()) {
+				System.out.println(scanner.nextLine());
 			}
-		} catch (FileNotFoundException e1) {
+			System.out.println("Tchau tchau.");
+			scanner.close();
+
+		} catch (
+
+		FileNotFoundException e1) {
 			System.out.println("Arquivo não pode ser encontrado. Verifique.\n" + e1.getMessage());
 		}
 	}
