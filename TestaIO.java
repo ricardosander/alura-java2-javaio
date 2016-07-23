cbdeb9263ef7f2ee565637946337f61490a50a3a
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-23 01:53:25 -0300

    Módulo 4, Exercício 3: lendo direto do arquivo.

--- TestaIO.java
+++ TestaIO.java
@@ -1,36 +1,43 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class TestaIO {
 
 	public static void main(String[] args) {
-		
-		InputStreamReader is = new InputStreamReader(System.in);
-		BufferedReader br = new BufferedReader(is);
-		System.out.println("Olá. Bem vindo ao leitor!!!");
+
+		FileInputStream is;
 		try {
 			
-			String texto;
-			do {
-				
-				System.out.println("Digite algo para ser lido. Digite \"sair\" para sair.");
-				texto = br.readLine();
-				
-				if (texto != null && !texto.equals("sair")) {
-					System.out.println("Texto lido: " + texto);
-				}
-			} while (texto != null && !texto.equals("sair"));
-		} catch (IOException e) {
-			System.out.println("Houve um erro ao realizar a leitura.\n" + e.getMessage());
-		} finally {
-			
-			System.out.println("Tchau tchau.");
+			is = new FileInputStream("src/arquivo.txt");
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			System.out.println("Olá. Bem vindo ao leitor de arquivo. Lendo arquivo.txt...");
 			try {
-				br.close();
+
+				StringBuilder textoBuilder = new StringBuilder();
+				String texto;
+				while((texto = br.readLine()) != null) {
+					textoBuilder.append(texto);
+					textoBuilder.append("\n");
+				}
+				System.out.println("Texto lido:");
+				System.out.println(textoBuilder.toString());
 			} catch (IOException e) {
-				System.out.println("Houve um erro ao fechar o leitor.\n" + e.getMessage());
+				System.out.println("Houve um erro ao realizar a leitura.\n" + e.getMessage());
+			} finally {
+
+				System.out.println("Tchau tchau.");
+				try {
+					br.close();
+				} catch (IOException e) {
+					System.out.println("Houve um erro ao fechar o leitor.\n" + e.getMessage());
+				}
 			}
+		} catch (FileNotFoundException e1) {
+			System.out.println("Arquivo não pode ser encontrado. Verifique.\n" + e1.getMessage());
 		}
 	}
 }
