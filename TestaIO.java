f90ae9e2919f4fb42c33e114a76be400e916ebc9
ricardosander <ricardo.sander.lopes@gmail.com>
2016-07-23 02:07:04 -0300

    Módulo 4, Exercício 6: usando classe Scanner para ler o teclado.

--- TestaIO.java
+++ TestaIO.java
@@ -1,29 +1,18 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class TestaIO {
 
 	public static void main(String[] args) {
 
-		try {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Olá. Bem vindo ao leitor de arquivo. Lendo arquivo.txt...");
 
-			InputStream is = new FileInputStream("src/arquivo.txt");
-			Scanner scanner = new Scanner(is);
-			System.out.println("Olá. Bem vindo ao leitor de arquivo. Lendo arquivo.txt...");
-
-			System.out.println("Texto lido:");
-			while (scanner.hasNextLine()) {
-				System.out.println(scanner.nextLine());
-			}
-			System.out.println("Tchau tchau.");
-			scanner.close();
-
-		} catch (
-
-		FileNotFoundException e1) {
-			System.out.println("Arquivo não pode ser encontrado. Verifique.\n" + e1.getMessage());
+		System.out.println("Texto lido:");
+		while (scanner.hasNextLine()) {
+			System.out.println(scanner.nextLine());
 		}
+		System.out.println("Tchau tchau.");
+		scanner.close();
+
 	}
 }
